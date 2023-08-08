fb7fa0ec190b82e397f20f55dc82462dcc2e1584
Ikhvan <ikhvan95@gmail.com>
2023-08-08 19:39:50 +0200

    Har lavet opdatering til koden, ny method: ThreeIncreasingAdjacent

--- Multiplication Table/Program.cs
+++ Multiplication Table/Program.cs
@@ -6,12 +6,18 @@ namespace Multiplication_Table
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Multiplication Tabe:");
+            Console.WriteLine("Multiplication Table:");
             PrintMultiplicationTable(10);
 
 
             int[] TheBiggestNumberList = new int[] { 190, 291, 145, 209, 280, 200 };
             Console.WriteLine(BiggestNumber(TheBiggestNumberList));
+
+
+            int[] threeIncreasingAdjacent = new int[] { 23, 48, 19, 20, 21, 45 };
+            Console.WriteLine(ThreeIncreasingAdjacent(threeIncreasingAdjacent));
+
+
         }
 
         public static void PrintMultiplicationTable(int size)
@@ -42,6 +48,24 @@ namespace Multiplication_Table
         }
 
 
+
+        static bool ThreeIncreasingAdjacent(int[] numbers)
+        {
+            if (numbers.Length < 3)
+            {
+                return false; // Vil return false frundet at der ikke er nok tal
+            }
+
+            for (int i = 2; i < numbers.Length; i++)
+            {
+                if (numbers[i] == numbers[i - 1] + 1 && numbers[i - 1] == numbers[i - 2] + 1)
+                {
+                    return true; // Vil return true hvis den finder de 3 tal
+                }
+            }
+
+            return false; // Vil return false, hvis det ikke blev fundet
+        }
 
 
 
